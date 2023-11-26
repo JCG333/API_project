b25ecd92527dc9fb41761d952ecbc31978b7f2d8
Jay <jusgav-1@student.ltu.se>
2023-11-26 19:15:55 +0100

    Added web to api request and response functionality
    - created web server using Nginx
    - created server for application using Unicorn
    - Added ability to query API from static web page.

--- app.py
+++ app.py
@@ -1,11 +1,17 @@
-from flask import Flask, request, jsonify, make_response
+from flask import Flask, request, jsonify, make_response, render_template
 from flask_sqlalchemy import SQLAlchemy
 from os import environ
+<<<<<<< Updated upstream
+=======
+import xmltodict # for xml convert
+from flask_cors import CORS #cross origin resource sharing
+>>>>>>> Stashed changes
 
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('DB_URL')
 db = SQLAlchemy(app)
+CORS(app) #cross origin resource sharing for flask app
 
 class User(db.Model):
     __tablename__ = 'users'
@@ -30,7 +36,7 @@ def test():
     return make_response(jsonify({'message': 'test route'}), 200)
 
 #create user
-@app.route('/users', methods=['POST'])
+@app.route('/api/users', methods=['POST'])
 def create_user():
     try:
         data = request.get_json()
@@ -42,7 +48,7 @@ def create_user():
         return make_response(jsonify({'message': 'error creating user'}), 500)
     
 #get all users
-@app.route('/users', methods=['GET'])
+@app.route('/api/users', methods=['GET'])
 def get_users():
     try:
         users = User.query.all() # query users
@@ -51,7 +57,7 @@ def get_users():
         return make_response(jsonify({'message': 'error getting users'}), 500)
     
 #get a user by id
-@app.route('/users/<int:id>', methods=['GET'])
+@app.route('/api/users/<int:id>', methods=['GET'])
 def get_user(id):
     try:
         user = User.query.filter_by(id=id).first() # query user filtered by id
@@ -62,7 +68,7 @@ def get_user(id):
         return make_response(jsonify({'message': 'error getting user'}), 500)
     
 #update a user
-@app.route('/users/<int:id>', methods=['PUT'])
+@app.route('/api/users/<int:id>', methods=['PUT'])
 def update_user(id):
     try:
         user = User.query.filter_by(id=id).first() # get user
@@ -77,7 +83,7 @@ def update_user(id):
         return make_response(jsonify({'message': 'error updating user'}), 500)
     
 #delete user
-@app.route('/users/<int:id>', methods=['DELETE'])
+@app.route('/api/users/<int:id>', methods=['DELETE'])
 def delete_user(id):
     try:
         user = User.query.filter_by(id=id).first() # get user
@@ -88,3 +94,7 @@ def delete_user(id):
         return make_response(jsonify({'message': 'user not found'}), 404)
     except Exception as e:
         return make_response(jsonify({'message': 'error deleting user'}), 500)
+    
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=4000)
