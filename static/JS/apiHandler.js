249a6fa4eb7b9946911c521cc3326dbe84edb208
Jay <jusgav-1@student.ltu.se>
2023-12-02 16:48:44 +0100

    api js functionality changes and UX design changes

--- static/JS/apiHandler.js
+++ static/JS/apiHandler.js
@@ -8,13 +8,26 @@ function get_users_data() {
             return response.json();
         })
         .then(data => {
-            document.getElementById('get_users_data').textContent = JSON.stringify(data);
+            //console.log(data);
+            let users = '<table>';
+            users += '<tr><th>ID</th><th>Username</th><th>Email</th></tr>'; // table headers
+            data.users.forEach(user => {
+                users += `
+                    <tr>
+                        <td>${user.id}</td>
+                        <td>${user.username}</td>
+                        <td>${user.email}</td>
+                    </tr>
+                `;
+            });
+            users += '</table>';
+            document.getElementById('data_display').innerHTML = users;
         })
         .catch(error => {
             console.error('There was a problem with the fetch operation of users:', error);
         });
 }
-document.getElementById('get_users_button').addEventListener('click', get_users_data);
+//document.getElementById('get_users_button').addEventListener('click', get_users_data);
 
 // get user
 function get_user_data(user_id) {
@@ -32,79 +45,96 @@ function get_user_data(user_id) {
         });
 }
 // event lisnter for get user button
+/*
 document.getElementById('get_user_button').addEventListener('click', function () {
     var user_id = document.getElementById('user_id_input').value;
     get_user_data(user_id);
 });
+*/
 
-// post user
-function post_user(user_name, user_email) {
+// ====== create user ====== 
+function create_user(user_name, user_email) {
     fetch('api/users',
         {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({ email: user_email, username: user_name })
         })
+        // check if response is OK
         .then(response => {
             if (!response.ok) {
-                throw new Error(`HTTP error! status: ${response.status}`);
+                return response.json().then(error => Promise.reject(error));
             }
-            return document.getElementById('post_user-creation_action_response').textContent = 'User created successfully!';
+            return response.json();
         })
+        // if OK
+        .then(data => {
+            document.getElementById('create_user_action_response').textContent = 'User created successfully!';
+            get_users_data();
+        })
+        // Error handling
         .catch(error => {
-            error.json().then(error => {
-                document.getElementById('post_user-creation_action_response').textContent = error.message;
-            })
+            document.getElementById('create_user_action_response').textContent = error;
         });
 }
-// event lisnter for post user button
+// event lisnter for CREATE user button
 document.getElementById('create_user_button').addEventListener('click', function () {
     var user_name = document.getElementById('user_name_input-post').value;
     var user_email = document.getElementById('user_email_input-post').value;
-    post_user(user_name, user_email);
+    create_user(user_name, user_email);
 });
 
-// update user
-function put_user(user_name, user_email, user_id) {
+// ====== update user ====== 
+function update_user(user_name, user_email, user_id) {
     fetch(`api/users/${user_id}`,
         {
             method: 'PUT',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({ email: user_email, username: user_name })
         })
+        // check if response is OK
         .then(response => {
             if (!response.ok) {
-                return response.json().then(error => {
-                    throw new Error(error.message);
-                });
+                return response.json().then(error => Promise.reject(error));
             }
-            return document.getElementById('put_user-update_action_response').textContent = 'User updated successfully!';
+            return response.json();
         })
+        // if OK
+        .then(data => {
+            document.getElementById('update_user_action_response').textContent = 'User updated successfully!';
+            get_users_data();
+        })
+        // Error handling
         .catch(error => {
-            document.getElementById('put_user-update_action_response').textContent = error.message;
+            document.getElementById('update_user_action_response').textContent = error;
         });
 }
-// event lisnter for put user button
+// event lisnter for UPDATE user button
 document.getElementById('update_user_button').addEventListener('click', function () {
     var user_name = document.getElementById('user_name_input-put').value;
     var user_email = document.getElementById('user_email_input-put').value;
     var user_id = document.getElementById('user_id_input-put').value;
-    put_user(user_name, user_email, user_id);
+    update_user(user_name, user_email, user_id);
 });
 
-// delete user
+// ====== delete user ====== 
 function delete_user(user_id) {
     fetch(`api/users/${user_id}`, { method: 'DELETE' })
+        // check if response is OK
         .then(response => {
             if (!response.ok) {
-                return response.json().then(error => {
-                    throw new Error(error.message);
-                });
+                return response.json().then(error => Promise.reject(error));
             }
-            document.getElementById('delete_user_action_response').textContent = 'User deleted successfully!';
+            return response.json();
         })
+        // if OK
+        .then(data => {
+            document.getElementById('delete_user_action_response').textContent = 'User deleted successfully!';
+            get_users_data();
+        })
+        // Error handling
         .catch(error => {
-            document.getElementById('delete_user_action_response').textContent = error.message;
+            document.getElementById('delete_user_action_response').textContent = error;
         });
 
 }
@@ -113,3 +143,6 @@ document.getElementById('delete_user_button').addEventListener('click', function
     var user_id = document.getElementById('user_id_input-delete').value;
     delete_user(user_id);
 });
+
+// get all users on page load
+get_users_data();
